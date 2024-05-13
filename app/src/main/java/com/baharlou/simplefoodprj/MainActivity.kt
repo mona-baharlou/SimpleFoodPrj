0c4e6af67a630f0874ebca7b7e1c017df90fd94b
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-13 23:07:13 +0330

    Initial commit

---
+++ app/src/main/java/com/baharlou/simplefoodprj/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.baharlou.simplefoodprj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
