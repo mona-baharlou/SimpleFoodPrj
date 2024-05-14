1f03101c218165ba28f5cfc73a49ac8db0b0a295
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-14 19:28:15 +0330

    MainActivity.kt updated

--- app/src/main/java/com/baharlou/simplefoodprj/Food.kt
+++ app/src/main/java/com/baharlou/simplefoodprj/Food.kt
@@ -5,10 +5,12 @@ data class Food(
     val txtSubject:String,
     val txtPrice:String,
     val txtDistance:String,
-    val txtRate:String,
     val txtCity:String,
     val imgUrl:String,
     val numOfRating:Int,
     val rating:Float,
+
+    //val txtRate:String,
+
 
 )
